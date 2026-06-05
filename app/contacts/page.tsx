f1c9e1da90bf07ae62f506bb851a8df810abb1fe
Airdpx <airdpx@gmail.com>
2026-06-05 07:25:14 +0300

    Trim contacts page explanatory copy

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -11,19 +11,7 @@ export default async function ContactsPage() {
     <StaticPageShell
       kicker="Контакты"
       title="Контакты"
-      intro="Напиши нам прямо из формы. Сообщение уйдёт через сервер на адрес, который администратор укажет в настройках сайта, и сохранится в базе."
-      aside={(
-        <div className="contact-list">
-          <div>
-            <b>Когда писать</b>
-            <span>Если нужен отклик по продукту, идее, ошибке, предложению или теме сотрудничества.</span>
-          </div>
-          <div>
-            <b>Как это работает</b>
-            <span>Форма отправляет письмо через сервер, а адрес получателя задаётся в админке.</span>
-          </div>
-        </div>
-      )}
+      intro="Напиши нам прямо из формы."
     >
       <ContactForm recipientEmail={contactEmail} />
     </StaticPageShell>
